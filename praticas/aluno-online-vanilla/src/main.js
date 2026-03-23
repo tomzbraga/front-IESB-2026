8727f7aa239f3cc45aa06f6e2f8d949a57403d2b
Lucas Tomaz <tomzbraga@gmail.com>
2026-03-23 10:25:39 -0300

    Organizando para criação da branch nova

--- praticas/aluno-online-vanilla/src/main.js
+++ praticas/aluno-online-vanilla/src/main.js
@@ -1,1 +1,37 @@
 import './style.css'
+
+import './style.css'
+
+const matricula = document.querySelector('#matricula');
+const senha     = document.querySelector('#senha');
+const botao     = document.querySelector('button');
+
+botao.addEventListener('click', function(e) {
+
+    const matriculaErro = document.querySelector("#matriculaErro");
+    const senhaErro     = document.querySelector("#senhaErro");
+
+    matriculaErro.textContent = '';
+    senhaErro.textContent = '';
+
+
+    if (matricula.value == '') {
+        matriculaErro.textContent = "Matricula é obrigatória";
+        return;
+    }
+
+    if (parseInt(matricula.value) <= 0) {
+        matriculaErro.textContent = "Matrícula inválida";
+        return;
+    }
+
+    if (senha.value == '') {
+        senhaErro.textContent = "Senha é obrigatória";
+        return;
+    }
+
+    if (senha.value.length < 8) {
+        senhaErro.textContent = "Senha deve conter no mínimo 8 caracteres";
+        return;
+    }
+});
