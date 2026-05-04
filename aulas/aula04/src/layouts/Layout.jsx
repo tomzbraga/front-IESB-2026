b78b624ce55a1998e5ce7fcabb47adc1497a802b
Lucas <tomzbraga@gmail.com>
2026-05-04 10:41:07 -0300

    aprendizado sobre react-router, cria pasta aula04

---
+++ aulas/aula04/src/layouts/Layout.jsx
@@ -0,0 +1,26 @@
+import Menu from '../components/Menu';
+import { Outlet } from 'react-router';
+
+function Layout() {
+
+  return ( 
+  
+  <>
+  
+    <aside>
+
+      <Menu />
+
+    </aside>
+
+    <main>
+
+      <Outlet />
+
+    </main>
+
+  </>
+
+)};
+
+export default Layout;
