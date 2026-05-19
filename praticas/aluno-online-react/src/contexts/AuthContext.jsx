2bd12a588ff711cc2cf9d86da95e88a4e8f0c4eb
Lucas <tomzbraga@gmail.com>
2026-05-19 11:02:33 -0300

    aula04

---
+++ praticas/aluno-online-react/src/contexts/AuthContext.jsx
@@ -0,0 +1,32 @@
+import { createContext, useState, useContext } from "react";
+
+const AuthContext = createContext();
+
+function AuthProvider({ children }) {
+  const [usuario, setUsuario] = useState({});
+  const [logado, setLogado] = useState(false);
+
+  const login = (dados) => {
+    // Chama a API do backend
+
+    setUsuario({ nome: "Jose" });
+    setLogado(true);
+  };
+
+  const logout = () => {
+    setUsuario({});
+    setLogado(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ logado, usuario, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+function useAuth() {
+  return useContext(AuthContext);
+}
+
+export { AuthProvider, useAuth };
